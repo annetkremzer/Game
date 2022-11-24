df8463265f9d455d7f19583506323c3befb18628
Anna <kreemzer.anet@gmail.com>
2022-11-24 18:41:11 +0300

    userReducer

--- frontend/src/App/api.ts
+++ frontend/src/App/api.ts
@@ -1,3 +1,4 @@
+import User, { Res } from "../features/reducers/userReducer/types/User";
 import Topic from "../features/Topic/types/Topic";
 
 export const loadTopics = async (): Promise<Topic[]> => {
@@ -5,17 +6,17 @@ export const loadTopics = async (): Promise<Topic[]> => {
   return res.json();
 };
 
-// export const registration = async (item: User): Promise<Res> => {
-//   const res = await fetch('http://localhost:4000/auth/registration', {
-//     method: 'post',
-//     headers: { 'Content-type': 'application/json' },
-//     credentials: 'include',
-//     body: JSON.stringify(item),
-//   });
-//   return res.json();
-// };
+export const registration = async (item: User): Promise<Res> => {
+  const res = await fetch('http://localhost:4000/auth/registration', {
+    method: 'post',
+    headers: { 'Content-type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify(item),
+  });
+  return res.json();
+};
 
-// export const logout = async (): Promise<Res> => {
-//   const res = await fetch('http://localhost:4000/auth/logout');
-//   return res.json();
-// };
+export const logout = async (): Promise<Res> => {
+  const res = await fetch('http://localhost:4000/auth/logout');
+  return res.json();
+};
---
+++ frontend/src/features/reducers/userReducer/types/User.ts
@@ -0,0 +1,6 @@
+export interface Res { message:string; user:string }
+
+export default interface User {
+  login:string;
+  password:string;
+}
