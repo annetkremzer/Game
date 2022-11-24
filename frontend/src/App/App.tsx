75dabf71b2822e7b5d56b06076071985a51fe311
Anna <kreemzer.anet@gmail.com>
2022-11-24 19:03:35 +0300

    header

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as api from './api'
 import { RootState } from '../features/reducers/store';
+import { Route, Routes } from 'react-router-dom';
+import Auth from '../features/auth/Auth';
+import Header from '../features/Header/Header';
 
 function App() {
 
@@ -9,11 +12,17 @@ function App() {
   useEffect(() => {
     api.loadTopics().then((res) => dispatch({ type: 'INIT_TOPIC', payload: res }))
   }, [])
-  const {topicState} = useSelector((state:RootState) => state)
+
+
+  const { topicState } = useSelector((state: RootState) => state)
   console.log(topicState, '----------')
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        {/* <Route path="main" element={<Main />} /> */}
+        <Route path="auth/:name" element={<Auth />} />
+      </Route>
+    </Routes>
   );
 }
 
