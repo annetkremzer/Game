957d19d80ae0f975b71feb5ceddb2e2596cefb36
Dmitry Denisov <double.dmt@yandex.ru>
2022-11-24 16:12:09 +0300

    edit back

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: ['http://localhost:3000'],credentials: true}));
+app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(session(sessionConfig));
