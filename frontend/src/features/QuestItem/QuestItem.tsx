16f898273fe9ad37a041215882bfd20707cd5e07
Alexandra <lubnina.alexandra@gmail.com>
2022-11-25 12:36:49 +0300

    realised function

--- frontend/src/features/QuestItem/QuestItem.tsx
+++ frontend/src/features/QuestItem/QuestItem.tsx
@@ -1,27 +1,50 @@
 import { Box, Button, Modal, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import Quest from './types/Quest'
 
 function QuestItem({ questBlock }: { questBlock: Quest }): JSX.Element {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [score, setScore] = useState(String(questBlock.score));
+  const [answer, setAnswer] = useState('');
+  const [checkAnswer, setCheckAnswer] = useState('');
+
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
+  const handleClose = () => {
+    setOpen(false);
+    setScore('');
+  };
+
+  const handleAnswer = () => {
+    if(answer.toLowerCase() === questBlock.answer.toLowerCase()) {
+      setCheckAnswer('Правильный ответ! Умничка! Еще чуть-чуть и ты Хокаге!')
+    } else {
+      setCheckAnswer(`Не правильно! Правильный ответ: ${questBlock.answer}`)
+    }
+  }
 
   const style = {
     position: 'absolute' as 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 1000,
+    height: 500,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
   };
 
   return (
     <div>
-      <Button onClick={handleOpen}>{questBlock.score}</Button>
+      {score ? <Button onClick={handleOpen}>{score}</Button> : <Button disabled></Button>}
+      
       <Modal
         open={open}
         onClose={handleClose}
@@ -29,12 +52,12 @@ function QuestItem({ questBlock }: { questBlock: Quest }): JSX.Element {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id="modal-modal-title" variant="h3" component="h2" >
             {questBlock.question}
           </Typography>
-          <TextField id="standard-basic" label="Answer" variant="standard" />
-          <Button >Answer</Button>
-
+          <TextField id="standard-basic" label="Answer" variant="standard" onChange={(e) => setAnswer(e.target.value)} value={answer}/>
+          <Button onClick={handleAnswer}>Answer</Button>
+          <p>{checkAnswer}</p>
         </Box>
       </Modal>
 
