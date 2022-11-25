c2a983a350c2aed6479581e7fa6117fce7b15222
Anna <kreemzer.anet@gmail.com>
2022-11-25 14:46:17 +0300

    addMainCss

--- frontend/src/features/Main/Main.tsx
+++ frontend/src/features/Main/Main.tsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { RootState } from '../reducers/store'
 import TopicList from '../TopicList/TopicList'
+import './Main.css'
+import regimg from './regimg.jpg'
 
 function Main() {
   const { user } = useSelector((state: RootState) => state.userState);
@@ -10,8 +12,12 @@ function Main() {
   return (
     <>
       {!user ? (
-      <div>
-        <h1>Для того, чтобы начать играть, Вам надо зарегестрироваться</h1>
+      <div className='container'>
+        <h2>Для начала зарегистрируйся, падаван!</h2>
+        <div className='img_cont'>
+          <img src={regimg} alt='teamkrs' width='400px' height='300px' />
+        </div>
+        <p>Ну или залогинься...</p>
       </div>
       ) : (
         <TopicList />
